374745b2b1c676070dad4bb92b62ba0c79255ba6
hgwak <hannelgwak@gmail.com>
2019-02-28 12:13:15 -0800

    changed folder name

---
+++ server/DummyData.js
@@ -0,0 +1,52 @@
+
+var crimeData = {
+    success: true,
+    data:[{
+            idDrNumber: 190506343,
+            crimeCode: {
+                code:624,
+                crimeType: 'Violent',
+                crimeDescription: 'Battery - Simple Assault',
+            },
+            areaId: 05,
+            longitude: 33.7941,
+            latitude: -118.29,
+            dateOccurred: '2019-23-02',
+            timeOccurred: '02:40',
+            dateCreated: '2019-23-02 02:40'
+        },
+        {
+            idDrNumber: 192006589,
+            crimeCode: {
+                code:624,
+                crimeType: 'Violent',
+                crimeDescription: 'Battery - Simple Assault'
+            },
+            areaId: {
+                id: 5,
+                name: 'some block'
+            },
+            longitude: 34.0654,
+            latitude:  34.0654,
+            dateOccurred: 2019-23-02,
+            timeOccurred: '20:27',
+            dateCreated: '2019-23-02 02:40'
+        },
+        {
+            idDrNumber: 190307371,
+            crimeCode: {
+                code:230,
+                crimeType: 'Violent',
+                crimeDescription: 'ASSAULT WITH DEADLY WEAPON, AGGRAVATED ASSAULT'
+            },
+            areaId: {
+                id: 5,
+                name:'some block'
+            },
+            longitude: 34.0268,
+            latitude:  -118.2808,
+            dateOccurred: 2019-23-02,
+            timeOccurred: '00:08',
+            dateCreated: '2019-23-02 02:40'
+        }
+]}
