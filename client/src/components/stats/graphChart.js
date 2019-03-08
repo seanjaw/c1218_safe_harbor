ce7deb9b88e098ba4c84e687432f21cd81cbe289
hgwak <hannelgwak@gmail.com>
2019-03-08 12:19:49 -0800

    created endpoint for crimes by month per year

--- client/src/components/stats/graphChart.js
+++ client/src/components/stats/graphChart.js
@@ -1,21 +1,55 @@
 import React, {Component} from 'react';
 import Chart from 'chart.js';
+import axios from 'axios';
 
 class GraphChart extends Component {
+    randomizer(){
+        let r = Math.floor(Math.random()*256);
+        let b = Math.floor(Math.random()*256);
+        let g = Math.floor(Math.random()*256);
+        return `rbg(${r},${b},${g})`
+    }
     async componentDidMount(){
+        const stats = await axios.get('/api/stats/linegraph');
+        console.log(stats)
         var ctx = document.getElementById('graphChart');
         var myChart = new Chart(ctx,{
             type: 'line',
             data: {
                 labels:['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
                 datasets:[{
-                    label: 'Crimes by Year',
+                    label: '2019',
+                    data: [12000,15678,13294,9023,13643,10000,15387, 8000, 11000,15678,13294,9023],
+                    borderColor: this.randomizer(),
+                    backgroundColor: 'rgba(0,0,0,0)',
+                    borderWidth: 3,
+                    lineTension:0
+                },
+                {
+                    label: '2018',
                     data: [12000,15678,13294,9023,13643,10000,15387, 8000, 11000,15678,13294,9023],
                     borderColor: 'rgb(255,99,132)',
                     backgroundColor: 'rgba(0,0,0,0)',
                     borderWidth: 3,
                     lineTension:0
-                }]
+                },
+                {
+                    label: '2017',
+                    data: [12000,15678,13294,9023,13643,10000,15387, 8000, 11000,15678,13294,9023],
+                    borderColor: 'rgb(255,99,132)',
+                    backgroundColor: 'rgba(0,0,0,0)',
+                    borderWidth: 3,
+                    lineTension:0
+                },
+                {
+                    label: '2016',
+                    data: [12000,15678,13294,9023,13643,10000,15387, 8000, 11000,15678,13294,9023],
+                    borderColor: 'rgb(255,99,132)',
+                    backgroundColor: 'rgba(0,0,0,0)',
+                    borderWidth: 3,
+                    lineTension:0
+                }
+            ]
             },
             options: {
                 scales: {
