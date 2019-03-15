19182a8e7ef8467e90bc7c18f25829e4178b90d9
anthonybo <twandeez@gmail.com>
2019-03-14 18:07:50 -0700

    Completed fixing the hover functionality for the icon on the 404 page

--- client/src/components/general/error_404/index.js
+++ client/src/components/general/error_404/index.js
@@ -5,12 +5,25 @@ import './style.scss';
 
 class Error404 extends Component {
 
+    componentDidMount() {
+        document.getElementById('goHome404').addEventListener('touchstart', this.iconOnHoverColor);
+        document.getElementById('goHome404').addEventListener('touchend', this.iconOffHoverColor);
+    }
+
+    iconOnHoverColor() {
+        this.setAttribute('style', 'color: #910c08')
+    }
+
+    iconOffHoverColor() {
+        this.setAttribute('style', '')
+    }
+
     render(){
         return (
             <div className='error-container'>
                 <img src={image404} alt="" className='error-gif'/>
                 <Link to='/'>
-                    <i className='material-icons'>home</i>
+                    <i className='material-icons' id='goHome404'>home</i>
                 </Link>
             </div>
         )
