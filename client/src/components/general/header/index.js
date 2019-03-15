8244471f6eb66442d3551db337360ce8f6404a7f
anthonybo <twandeez@gmail.com>
2019-03-14 22:36:05 -0700

    Added a logo instead of text and used a div and added it as the background for that div

--- client/src/components/general/header/index.js
+++ client/src/components/general/header/index.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import './style.css';
+import logo from './logo-w.png';
 
 class AppHeader extends Component {
+    componentDidMount() {
+        let target = document.getElementById('logo');
+        target.setAttribute("style", "background-image: url(" + logo + ");background-repeat: no-repeat;background-size: contain");
+    }
 
     render(){
         return(
-            <h1 className='appHeader'>Safe Harbor</h1>
+            <div className='headerLogo' id='logo'></div>
         )
     }
 }
