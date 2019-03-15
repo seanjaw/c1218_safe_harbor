a6ff245dcca8254f26aa2f811eb36fbe87569c37
anthonybo <twandeez@gmail.com>
2019-03-14 18:57:36 -0700

    Add a header placeholder and fix font for the crimStats on mobile

---
+++ client/src/components/general/header/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './style.css';
+
+class AppHeader extends Component {
+
+    render(){
+        return(
+            <h1 className='appHeader'>Safe Harbor</h1>
+        )
+    }
+}
+
+export default AppHeader;
